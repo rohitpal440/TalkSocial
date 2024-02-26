40ca5304273ad3116674eadc7e8885af9848c501
Rohit Pal <rohitpal440@gmail.com>
2024-02-26 14:21:41 +0530

    Initial Commit

---
+++ TalkSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkSocial
+//
+//  Created by Rohit Pal on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
